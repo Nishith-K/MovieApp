b2d21d0e12bee0f83529e6c06e6b2f74cf085d5b
Nishith-K <69963317+Nishith-K@users.noreply.github.com>
2020-09-24 20:02:59 +0530

    Added initial Angular code

--- src/main/java/com/nk/Movie/Controller/MovieController.java
+++ src/main/java/com/nk/Movie/Controller/MovieController.java
@@ -1,19 +1,34 @@
 package com.nk.Movie.Controller;
 
-import com.nk.Movie.Service.MovieService;
+import com.nk.Movie.Entity.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nk.Movie.Service.MovieService;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.List;
+
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api")
 public class MovieController {
 
 	@Autowired
 	private MovieService movieService;
 
-	@GetMapping("/getMovies")
-	public String getResult() {
-		movieService.getMovies();
-		return "Movies!!";
+
+//	@GetMapping(value = {"/", "/index"})
+//	public String index(Model model) {
+//	    model.addAttribute("title", "MovieDB");
+//	    return "index";
+//	}
+	@GetMapping(value="/movies")
+	public List<Movie> getResult() {
+		return movieService.getMovies();
 	}
 }
--- src/main/java/com/nk/Movie/Repository/MovieRepository.java
+++ src/main/java/com/nk/Movie/Repository/MovieRepository.java
@@ -1,14 +1,12 @@
 package com.nk.Movie.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nk.Movie.Entity.Movie;
 
-import java.util.List;
-
 @Repository
-public interface MovieRepository extends CrudRepository<Movie,Long> {
+public abstract class MovieRepository implements CrudRepository<Movie,Long> {
+	
 
 }
