e0081ee002de7f1d91b9953f2d43b7bb4953e3c7
Nishith-K <69963317+Nishith-K@users.noreply.github.com>
2020-09-24 20:02:56 +0530

    Added initial Angular code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GetMoviesComponent } from './get-movies/get-movies.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { Router, RouterModule, Routes} from "@angular/router";
+
+const appRoutes :Routes = [
+  {
+    path:'movies',
+    component: GetMoviesComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetMoviesComponent,
+    MovieDetailsComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
